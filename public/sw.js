d1bf618109a841e71e2e985b64b51bed3d097ce6
Bruno Leandro <bldigitalagenciapt@gmail.com>
2026-03-12 13:27:41 +0000

    feat: Add a Puppeteer production test script and a service worker to address blank screen issues.

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,24 @@
-// Basic Service Worker for PWA installability
-const CACHE_NAME = 'voy-v1';
-const ASSETS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/manifest.json',
-    '/vite.svg'
-];
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+});
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS_TO_CACHE);
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    console.log('[Service Worker] Deleting old cache:', cacheName);
+                    return caches.delete(cacheName);
+                })
+            );
+        }).then(() => {
+            console.log('[Service Worker] Unregistering self to fix blank screen issue');
+            self.registration.unregister();
         })
     );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+    // Always go to the network, never use cache to prevent the "white screen" error
+    event.respondWith(fetch(event.request).catch(() => new Response("Network error - please check your connection.")));
 });
